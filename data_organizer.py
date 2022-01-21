8d27eea6928059af1f59e1c22b307bcc0988e403
jinyoungpark <jinyoung.park@vanderbilt.edu>
2022-01-22 00:02:34 +0900

    File path error fixed

--- data_organizer.py
+++ data_organizer.py
@@ -1,42 +1,66 @@
+
 """
 data_organizer.py
 
-Calls on data scraping modules to receive collections
-of products and organize them into a csv file
+This module passes on given inputs to modules that scrapes websites
+specified by the user and collects all returned information on 'scraped-products.csv'
 
 # Author: Jinyoung Park (parkj22)
-# Version: January 19, 2022
+# Version: January 21, 2022
 """
-
+import data_scraper_etsy
+import search_window
 from chrome_driver import ChromeDriver
 import data_scraper_amazon
 import data_scraper_ebay
-import data_scraper_target
 import csv
 from product_info import Product
 
 
-def organize():
+def organize(search, path, amazon, ebay, etsy, num_page):
     """
-    organize() creates a csv file and
+    organize(): handles user's input from the Tk window and passes
+    'search' and 'num_page' to appropriate scraper modules.
+    search: URL query parameter
+    path: directory path for the csv file 'scraped-products.csv'
+    amazon: boolean value for scraping on Amazon.com
+    ebay: boolean value for scraping on Ebay.com
+    etsy: boolean value for scraping on Etsy.com
+    num_page: total number of pages to be scraped
     """
 
-    filename = "scraped-products.csv"
+    # csv file is created at the path given by the user
+    filename = path + "/scraped-products.csv"
     f = open(filename, "w", encoding="utf8", newline="")
     writer = csv.writer(f)
 
     # Input the table header ["Website", "Name", ... , "Link"]
     writer.writerow(["Website"] + Product.get_description())
 
-    # extract_to_csv(writer, "Amazon", data_scraper_amazon.extract)
-    # extract_to_csv(writer, "Ebay", data_scraper_ebay.extract)
-    extract_to_csv(writer, "Walmart", data_scraper_target.extract)
+    # Pass on 'search' and 'num_page' to modules specified by the user
+    if amazon:
+        extract_to_csv(writer, "Amazon", data_scraper_amazon.extract, search, num_page)
+    if ebay:
+        extract_to_csv(writer, "Ebay", data_scraper_ebay.extract, search, num_page)
+    if etsy:
+        extract_to_csv(writer, "Etsy", data_scraper_etsy.extract, search, num_page)
 
     # Quit browser after collecting all data
     ChromeDriver.get_instance().quit()
 
+    # Info message is popped up when collecting / writing data is complete
+    search_window.notify_complete()
 
-def extract_to_csv(writer, website_str, extract):
-    products = extract()
+
+def extract_to_csv(writer, website_str, extract, search, num_page):
+    """
+    extract_to_csv(): writes returned information to the csv file
+    writer: csv writer object
+    website_str: Name of the website which the information is from
+    extract: scraping function from modules
+    search: URL query parameter
+    num_page: total number of pages to be scraped
+    """
+    products = extract(search, num_page)
     for product in products:
         writer.writerow([website_str] + product.to_list())
